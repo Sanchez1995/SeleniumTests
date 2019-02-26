3d89aad07ba1c454fbd043cf62a71be202f7b850
Sanchez1995 <https://github.com/Sanchez1995/>
2019-02-26 11:11:56 +0000

    new code

--- Selenium/src/test/java/com/qa/Selenium/SeleniumTests.java
+++ Selenium/src/test/java/com/qa/Selenium/SeleniumTests.java
@@ -1,5 +1,6 @@
 package com.qa.Selenium;
 
+import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,17 +12,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumTests {
 
 	WebDriver driver;
-
-//	WebElement checkElement = driver.findElement(By.xpath("//*[@id=\"hplogo\"]"));
-
 	
-	String url = "https://www.google.com/";
+	String X = "Sanchez";
+
+	String url = "http://thedemosite.co.uk/addauser.php";
 
 	@Before
 	public void setup() {
 
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Admin/Desktop/ExternalApps/chromedriver.exe");
 		driver = new ChromeDriver();
+
 	}
 
 	@After
@@ -33,6 +34,26 @@ public class SeleniumTests {
 	public void methodTest() {
 		driver.manage().window().maximize();
 		driver.get(url);
+	}
+
+	@Test
+	public void addUserTest() {
+
+		driver.get(url);
+		WebElement checkElementUsername = driver.findElement(By.xpath(
+				"/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[1]/td[2]/p/input"));
+		checkElementUsername.sendKeys("Sanchez");
+		WebElement checkElementPassword = driver.findElement(By.xpath(
+				"/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[2]/td[2]/p/input"));
+		checkElementPassword.sendKeys("Sanchez");
+		WebElement checkElementSave = driver.findElement(By.xpath(
+				"/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[3]/td[2]/p/input"));
+		checkElementSave.click();
+		WebElement checkElementBody = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/blockquote/blockquote[2]/blockquote"));
+		
+		assertEquals("sanchez not found", true, checkElementBody.getText().contains(X));
+		
+		/*System.out.println(checkElementBody.getText());*/
 	}
 
 }
